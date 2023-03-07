8202fb6230225a15e7e94ae82d40239576886b77
quocthinhe <lquocthinh10a1@gmail.com>
2023-03-08 00:35:20 +0700

    only one customer upload

--- bktel/resources/js/app.js
+++ bktel/resources/js/app.js
@@ -36,6 +36,7 @@ Vue.component('home-component', require('./components/HomeComponent.vue').defaul
 Vue.component('footer-component', require('./components/FooterComponent.vue').default);
 Vue.component('nav-bar-component', require('./components/navbarComponent.vue').default);
 Vue.component('take-photo-component', require('./components/TakePhotoComponent.vue').default);
+Vue.component('upload-customer-component', require('./components/UploadCustomerComponent.vue').default);
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
---
+++ bktel/resources/js/components/UploadCustomerComponent.vue
@@ -0,0 +1,31 @@
+<template>
+<div style="width: 60%; margin-left: 300px;"> 
+      <div >
+        <div class='center_form'>
+          <h2>Upload<span class="badge bg-secondary">Form</span></h2>
+          <label class='black' for="first_name">Customer Name</label>
+          <input name="first_name"  placeholder="ID" class="form-control " />
+        </div>
+        <div class='center_form'>
+          <label class='black' for="first_name">Customer Phone</label>
+          <input name="first_name"  placeholder="ID" class="form-control " />
+        </div>
+        <div class='center_form'>
+          <label class='black' for="first_name">Customer Image Path</label>
+          <input name="first_name"  placeholder="Semester" class="form-control " />
+        </div>
+
+        <div class="centerr">
+          <button class="btn btn-primary center_form centerr but_student custom-button margintop-40px"> Submit
+          </button>
+        </div>
+      </div>
+    </div>
+</template>
+
+
+
+<script>
+
+
+</script>
